5cf69fa7e0f9ea4e3f92fb14d467b8f844b56941
unknown <leo.shishir105@gmail.com>
2018-08-08 16:53:20 +0530

    Change prod url

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,7 +1,16 @@
 import axios from 'axios'
 
+function getEnvBaseURL () {
+  console.log(process.env.NODE_ENV)
+  if (process.env.NODE_ENV === 'production') {
+    return 'https://mevn-server.herokuapp.com/'
+  } else {
+    return 'http://localhost:3000/'
+  }
+}
+
 export default () => {
   return axios.create({
-    baseURL: 'http://localhost:3000/'
+    baseURL: getEnvBaseURL()
   })
 }
